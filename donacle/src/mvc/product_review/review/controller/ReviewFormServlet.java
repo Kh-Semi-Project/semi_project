07c05aa11fc19bb59d3fc45342c71ae3a369eadd
BCSJH <kimbh666@naver.com>
2021-11-06 21:33:54 +0900

    리뷰 수정

--- donacle/src/mvc/product_review/review/controller/ReviewFormServlet.java
+++ donacle/src/mvc/product_review/review/controller/ReviewFormServlet.java
@@ -1,19 +1,14 @@
 package mvc.product_review.review.controller;
-
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mvc.login_join_and_management.model.vo.Member;
 import mvc.product_review.review.model.service.ReviewService;
 import mvc.product_review.review.model.vo.BuyLog;
-
-
 /**
  * Servlet implementation class BoardCommentEnrollServlet
  */
@@ -21,21 +16,25 @@ import mvc.product_review.review.model.vo.BuyLog;
 public class ReviewFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ReviewService reviewService = new ReviewService();
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Member loginMember = ((Member)request.getSession().getAttribute("loginMember"));
-		String loginId = null;
-		if(loginMember != null) loginId = loginMember.getId();
-		String id = "test0"; //세션 정보 알아내서 수정하기!
-		System.out.println("테스트"+loginMember);
-		
-		List<BuyLog> list = reviewService.selectBuyLog(loginId);
-		System.out.println("리뷰 데이터"+list);
-		if(list != null && list.size() > 0) {
-			request.setAttribute("buyLogList", list);
-			request.getRequestDispatcher("/WEB-INF/views/product_review/reviewForm.jsp").forward(request, response);			
+
+		if(loginMember == null) {
+			String location = request.getContextPath() + "/";			
+			response.sendRedirect(location);
+				
 		}else {
-			request.getRequestDispatcher("/WEB-INF/views/product_review/message/noPurchase.jsp").forward(request, response);
-		}	
+			String loginId = loginMember.getId();
+			System.out.println("테스트"+loginMember);
+			
+			List<BuyLog> list = reviewService.selectBuyLog(loginId);
+			System.out.println("리뷰 데이터"+list);
+			if(list != null && list.size() > 0) {
+				request.setAttribute("buyLogList", list);
+				request.getRequestDispatcher("/WEB-INF/views/product_review/reviewForm.jsp").forward(request, response);			
+			}else {
+				request.getRequestDispatcher("/WEB-INF/views/product_review/message/noPurchase.jsp").forward(request, response);
+			}	
+		}
 	}
-}	
+}
